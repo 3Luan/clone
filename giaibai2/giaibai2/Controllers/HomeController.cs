d630f3c0e935e559197823e2a22f9726e57653c4
3Luan <nguyenthanhluanbntsbd@gmail.com>
2024-01-15 23:29:01 +0700

    add giaibai2

---
+++ giaibai2/giaibai2/Controllers/HomeController.cs
@@ -0,0 +1,159 @@
+﻿using giaibai2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace giaibai2.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLBanHangQuanAoEntities db = new QLBanHangQuanAoEntities();
+        public ActionResult Index()
+        {
+            ViewData["PhanLoaiPhu"] = db.PhanLoaiPhus.ToList();
+            ViewData["SanPham"] = db.SanPhams.ToList();
+            return View();
+        }
+
+        public ActionResult GetProductByCategory(string phanloai)
+        {
+            if (phanloai == "Tất cả sản phẩm")
+            {
+                var sanPham1 = db.SanPhams.ToList();
+
+                var _sanPham1 = sanPham1
+                .Select(sp => new SanPham
+                {
+                    MaSanPham = sp.MaSanPham,
+                    TenSanPham = sp.TenSanPham,
+                    DonGiaBanNhoNhat = sp.DonGiaBanNhoNhat,
+                    DonGiaBanLonNhat = sp.DonGiaBanLonNhat,
+                    TrangThai = sp.TrangThai,
+                    MoTaNgan = sp.MoTaNgan,
+                    AnhDaiDien = sp.AnhDaiDien,
+                    NoiBat = sp.NoiBat,
+                    MaPhanLoaiPhu = sp.MaPhanLoaiPhu,
+                    MaPhanLoai = sp.MaPhanLoai,
+                    GiaNhap = sp.GiaNhap
+                }).ToList();
+                return Json(new { sanPham = _sanPham1 }, JsonRequestBehavior.AllowGet);
+            }
+            var sanPham = db.SanPhams
+                .Where(sp => sp.PhanLoaiPhu.TenPhanLoaiPhu == phanloai)
+                .ToList();
+
+            // tạo danh sách mới chỉ lấy các thuộc tính
+            // không lấy các đối tượng tham chiếu
+            var _sanPham = sanPham
+                .Select(sp => new SanPham
+                {
+                    MaSanPham = sp.MaSanPham,
+                    TenSanPham = sp.TenSanPham,
+                    DonGiaBanNhoNhat = sp.DonGiaBanNhoNhat,
+                    DonGiaBanLonNhat = sp.DonGiaBanLonNhat,
+                    TrangThai = sp.TrangThai,
+                    MoTaNgan = sp.MoTaNgan,
+                    AnhDaiDien = sp.AnhDaiDien,
+                    NoiBat = sp.NoiBat,
+                    MaPhanLoaiPhu = sp.MaPhanLoaiPhu,
+                    MaPhanLoai = sp.MaPhanLoai,
+                    GiaNhap = sp.GiaNhap
+                }).ToList();
+
+            return Json(new { sanPham = _sanPham }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        // GET: SanPhams/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaPhanLoai = new SelectList(db.PhanLoais, "MaPhanLoai", "PhanLoaiChinh");
+            ViewBag.MaPhanLoaiPhu = new SelectList(db.PhanLoaiPhus, "MaPhanLoaiPhu", "TenPhanLoaiPhu");
+            return View();
+        }
+
+        // POST: SanPhams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaSanPham,TenSanPham,MaPhanLoai,GiaNhap,DonGiaBanNhoNhat,DonGiaBanLonNhat,TrangThai,MoTaNgan,AnhDaiDien,NoiBat,MaPhanLoaiPhu")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SanPhams.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaPhanLoai = new SelectList(db.PhanLoais, "MaPhanLoai", "PhanLoaiChinh", sanPham.MaPhanLoai);
+            ViewBag.MaPhanLoaiPhu = new SelectList(db.PhanLoaiPhus, "MaPhanLoaiPhu", "TenPhanLoaiPhu", sanPham.MaPhanLoaiPhu);
+            return View(sanPham);
+        }
+
+        public ActionResult SuaSanPham(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaPhanLoai = new SelectList(db.PhanLoais, "MaPhanLoai", "PhanLoaiChinh", sanPham.MaPhanLoai);
+            ViewBag.MaPhanLoaiPhu = new SelectList(db.PhanLoaiPhus, "MaPhanLoaiPhu", "TenPhanLoaiPhu", sanPham.MaPhanLoaiPhu);
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SuaSanPham([Bind(Include = "MaSanPham,TenSanPham,MaPhanLoai,GiaNhap,DonGiaBanNhoNhat,DonGiaBanLonNhat,TrangThai,MoTaNgan,AnhDaiDien,NoiBat,MaPhanLoaiPhu")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaPhanLoai = new SelectList(db.PhanLoais, "MaPhanLoai", "PhanLoaiChinh", sanPham.MaPhanLoai);
+            ViewBag.MaPhanLoaiPhu = new SelectList(db.PhanLoaiPhus, "MaPhanLoaiPhu", "TenPhanLoaiPhu", sanPham.MaPhanLoaiPhu);
+            return View(sanPham);
+        }
+
+
+        // GET: SanPhams/Details/5
+        public ActionResult ChiTietSanPham(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Delete/5
+        [HttpPost, ActionName("ChiTietSanPham")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            SanPham sanPham = db.SanPhams.Find(id);
+            db.SanPhams.Remove(sanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
